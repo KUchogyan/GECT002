ef5e7f03917fae06ce4151dda18d5f1138708cd3
KUchogyan <chogyan@korea.ac.kr>
2025-06-27 12:26:09 +0900

    Delete PythonProject_test/endblabla.py

--- PythonProject_test/endblabla.py
+++
@@ -1,4 +0,0 @@
-name = input("your name again was..?")
-age = int(input("what's your age is?"))
-print("your names is", name, "and your age is", age)
-print("your age after ten years is", age + 10, ".")
